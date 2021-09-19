87aff74dbdfc40f06a69dafc4b6af7fa623cae1f
Kamindu99 <kamidugayantha123@gmail.com>
2021-09-19 22:53:05 +0530

    view details

--- app/src/main/java/com/example/restaurant_mobile_app/OrderComplete.java
+++ app/src/main/java/com/example/restaurant_mobile_app/OrderComplete.java
@@ -1,12 +1,22 @@
 package com.example.restaurant_mobile_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class OrderComplete extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,7 @@ public class OrderComplete extends AppCompatActivity {
         setTitle("Order Confirmation");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
 
